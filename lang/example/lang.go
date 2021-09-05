4221af107c8a5dd807be9fe4f8b39dcc9e6e8a97
gmvbr <87571591+gmvbr@users.noreply.github.com>
2021-09-05 17:14:51 -0400

    Update example

--- lang/example/lang.go
+++ lang/example/lang.go
@@ -1,14 +1,22 @@
-package help
+package hero
 
 type vehicle interface {
 }
 
-type cube struct {
-}
-
-type Car interface {
+type Task interface {
 }
 
 type Mesh struct {
+  Name string
+  X int
+  Y int
+  Z int
+}
+
+type Person struct {
+  Name string
+  Age int
+  Email string
+  Password string
 }
 
