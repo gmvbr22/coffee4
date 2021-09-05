fdeef7463ebbb6f498f5b0552962126d28d05335
gmvbr <87571591+gmvbr@users.noreply.github.com>
2021-09-05 11:08:58 -0400

    Create InterfaceType

---
+++ lang/src/lang/types/interface.ts
@@ -0,0 +1,26 @@
+import {pascalCase, snakeCase} from 'change-case';
+import {Builder, StringBuilder} from '../builder';
+
+export class InterfaceType implements Builder {
+  public readonly visible: boolean;
+  public readonly name: string;
+
+  constructor(visible: boolean, name: string) {
+    this.visible = visible;
+    this.name = name;
+  }
+
+  public build(builder: StringBuilder) {
+    builder.append('type ');
+    if (this.visible) {
+      builder.append(pascalCase(this.name));
+    } else {
+      builder.append(snakeCase(this.name));
+    }
+    builder.append(' interface {');
+    builder.newline();
+    builder.append('}');
+    builder.newline();
+    builder.newline();
+  }
+}
