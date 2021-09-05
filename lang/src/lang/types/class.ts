0bdd0c7c9d080d6374de9bfddc757688a401bc76
gmvbr <87571591+gmvbr@users.noreply.github.com>
2021-09-05 17:15:18 -0400

    Update ClassType

--- lang/src/lang/types/class.ts
+++ lang/src/lang/types/class.ts
@@ -1,13 +1,16 @@
 import {pascalCase, snakeCase} from 'change-case';
 import {Builder, StringBuilder} from '../builder';
+import {FieldType} from './field';
 
 export class ClassType implements Builder {
   public readonly visible: boolean;
   public readonly name: string;
+  public readonly fields: FieldType[];
 
-  constructor(visible: boolean, name: string) {
+  constructor(visible: boolean, name: string, fields: FieldType[]) {
     this.visible = visible;
     this.name = name;
+    this.fields = fields;
   }
 
   public build(builder: StringBuilder) {
@@ -19,6 +22,9 @@ export class ClassType implements Builder {
     }
     builder.append(' struct {');
     builder.newline();
+    for (const item of this.fields) {
+      item.build(builder, 1);
+    }
     builder.append('}');
     builder.newline();
     builder.newline();
