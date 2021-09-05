b04b654f0d83e81414797ab4960cf91ccdc95dc5
gmvbr <87571591+gmvbr@users.noreply.github.com>
2021-09-05 17:15:35 -0400

    Update LangBuilder

--- lang/src/lang/lang.ts
+++ lang/src/lang/lang.ts
@@ -1,23 +1,28 @@
 import {
   CoffeeClassContext,
   CoffeeInterfaceContext,
+  FieldDeclarationContext,
+  PrivateFieldsContext,
   PrivateTypesContext,
   ProgramContext,
+  PublicFieldsContext,
   PublicTypesContext,
 } from '../coffee4/Coffee4Parser';
 import {Builder} from './builder';
 import {ClassType} from './types/class';
+import {FieldType} from './types/field';
 import {InterfaceType} from './types/interface';
 import {ProgramType} from './types/program';
 
 export class LangBuilder {
   static parseProgram(node: ProgramContext): Builder {
-    const name = node.namespace().ID().text;
+    const name = node.namespace().PACKAGE_NAME().text;
     return new ProgramType(name, [
       ...LangBuilder.parsePrivateTypes(node.privateTypes()),
       ...LangBuilder.parsePublicTypes(node.publicTypes()),
     ]);
   }
+
   static parsePublicTypes(nodes: PublicTypesContext[]): Builder[] {
     const types = [];
     if (nodes !== undefined) {
@@ -49,13 +54,53 @@ export class LangBuilder {
   }
 
   static parseClassType(isExport: boolean, node: CoffeeClassContext): Builder {
-    return new ClassType(isExport, node.NAME().text);
+    return new ClassType(isExport, node.identifier().text, [
+      ...LangBuilder.publicFields(node.publicFields()),
+      ...LangBuilder.privateFields(node.privateFields()),
+    ]);
+  }
+
+  static publicFields(nodes: PublicFieldsContext[]): FieldType[] {
+    const types: FieldType[] = [];
+    if (nodes !== undefined) {
+      for (const type of nodes) {
+        let temp;
+        if ((temp = type.fieldDeclaration()) !== undefined) {
+          types.push(LangBuilder.fieldDeclaration(true, temp));
+        }
+      }
+    }
+    return types;
+  }
+
+  static privateFields(nodes: PrivateFieldsContext[]): FieldType[] {
+    const types: FieldType[] = [];
+    if (nodes !== undefined) {
+      for (const type of nodes) {
+        let temp;
+        if ((temp = type.fieldDeclaration()) !== undefined) {
+          types.push(LangBuilder.fieldDeclaration(true, temp));
+        }
+      }
+    }
+    return types;
+  }
+
+  static fieldDeclaration(
+    isExport: boolean,
+    node: FieldDeclarationContext
+  ): FieldType {
+    return new FieldType(
+      isExport,
+      node.identifier().text,
+      node.primitive().text
+    );
   }
 
   static parseInterfaceType(
     isExport: boolean,
     node: CoffeeInterfaceContext
   ): Builder {
-    return new InterfaceType(isExport, node.NAME().text);
+    return new InterfaceType(isExport, node.identifier().text);
   }
 }
